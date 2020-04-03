c63b22127082c019782d0509717d6bdb2f5a006d
RavenXavier <k.munro1998@gmail.com>
2020-04-02 21:29:07 -0400

    Deletion(complete)/Veiw Count(incomplete)

--- client/main.js
+++ client/main.js
@@ -19,39 +19,49 @@ Template.mainBody.helpers({
  	allCards(){
  		return booksdb.find();
  	},
+ 	veiws() {
+    return Template.instance().veiws.get();
+  },
 });
 
 Template.mainBody.events({
   'click .js-veiwBook'(event, instance) {
-    // open veiw pane
-    console.log('Veiwing...' + ":" + this._id);
     $("#veiwBookModal").modal("show");
     var myId = this._id;
   	var theTitle = booksdb.findOne({_id:myId}).title;
   	var thePath = booksdb.findOne({_id:myId}).path;
   	var theDesc = booksdb.findOne({_id:myId}).desc;
-    console.log('Veiwing...' + ":" + this._id+ 'Title: '+ theTitle + 'Desc: '+theDesc+'path: '+thePath); //all info appears 
+  	var theAuthor = booksdb.findOne({_id:myId}).author;
   	$("#veiwId").val(myId);
-  	$("#veiwTitle").html(theTitle); //no title
-  	$("#veiwDesc").html(theDesc); //no description
-  	$(".veiwHolder").attr("src", thePath); //image appears
-  	console.log($("#veiwTitle").val()); //title appars in log 
+  	$("#veiwTitle").html(theTitle); 											//shows title
+  	$("#veiwDesc").html(theDesc); 												//shows description
+  	$("#veiwAuthor").html(theAuthor);
+  	$(".veiwHolder").attr("src", thePath); 								//image appears
+  	instance.veiws.set(instance.veiws.get() + 1);
+  	
     },
+
   'click .js-editBook'(event, instance) {
-    // open edit pane
     console.log('Editing...'+ ":" + this._id);
     $("#editBookModal").modal("show");
   	var myId = this._id;
   	var theTitle = booksdb.findOne({_id:myId}).title;
   	var thePath = booksdb.findOne({_id:myId}).path;
   	var theDesc = booksdb.findOne({_id:myId}).desc;
+  	var theAuthor = booksdb.findOne({_id:myId}).author;
   	$("#editId").val(myId);
   	$("#editbookTitle").val(theTitle);
   	$("#editbookPath").val(thePath);
+  	$("#editbookAuthor").val(theAuthor);
   	$("#editbookDesc").val(theDesc);
   	$(".editHolder").attr("src", thePath);
  	},
-
+ 	'click .js-like'(event, instance){
+ 		console.log("like: " + this._id);
+ 	},
+ 	'click .js-dislike'(event, instance){
+ 		console.log("dislike: " + this._id);	
+ 	},
 });
 
 Template.addBook.events({
@@ -59,27 +69,29 @@ Template.addBook.events({
 		var myTitle = $('#bookTitle').val();
 		var myPath = $('#bookPath').val();
 		var myDesc = $('#bookDesc').val();
+		var myAuthor = $('#bookAuthor').val();
 		booksdb.insert({
 			"title": myTitle,
 			"path": myPath,
-			"desc": myDesc
+			"desc": myDesc,
+			"author": myAuthor
 		});
 		$("#addBookModal").modal("hide");
 		$('#bookTitle').val('');
 		$('#bookPath').val('');
+		$('#bookAuthor').val('');
 		$('#bookDesc').val('');
 	},
 	'click .js-closeMe'(event, instance){
 		$("#addBookModal").modal("hide");
 		$('#bookTitle').val('');
 		$('#bookPath').val('');
+		$('#bookAuthor').val('');
 		$('#bookDesc').val('');
 		$(".placeHolder").attr("src",$("#bookPath").val());
 	},
 	'input #bookPath'(event, instance){
 			$(".placeHolder").attr("src",$("#bookPath").val());
-			//.log($("#bookPath").val());
-
 		},
 
 });
@@ -89,44 +101,47 @@ Template.editBook.events({
 		var newTitle = $('#editbookTitle').val();
 		var newPath = $('#editbookPath').val();
 		var newDesc = $('#editbookDesc').val();
+		var newAuthor = $('#editAuthor').val();
 		var updateId = $('#editId').val();
 		booksdb.update({_id: updateId},
 				{$set:{
 					"title": newTitle,
 					"path": newPath,
-					"desc": newDesc
+					"desc": newDesc,
+					"author": newAuthor
 				}}
 			);
 		$("#editBookModal").modal("hide");
-		// console.log(newTitle + ": " + newDesc + ": " + newPath + ": " + updateId );
 	},
 	'click .js-closeMe'(event, instance){
 		$("#editBookModal").modal("hide");
 		$('#editbookTitle').val('');
 		$('#editbookPath').val('');
+		$('#bookAuthor').val('')
 		$('#editbookDesc').val('');
 		$(".editHolder").attr("src",$("#editbookPath").val());
 	},
 	'input #editbookPath'(event, instance){
 			$(".editHolder").attr("src",$("#editbookPath").val());
-			//console.log($("#editbookPath").val());
 		}	
 });
 
 Template.veiwBook.events({
 	'click .js-delete'(event, instance){
-		var myId = this._id;
+		var myId = $('#veiwId').val();
 		$("#deleteId").val(myId);
-		console.log(this._id);
 		$('#confirmModal').modal('show')
 	},
 	'click .js-confirm'(event, instance){
 		var myId = $('#deleteId').val();
-		console.log(myId);
  		$("#"+myId).fadeOut('slow',function(){
   	booksdb.remove({_id:myId});
   	});
-  	console.log(myId);
 	},
 
+});
+
+
+Template.mainBody.onCreated(function mainBodyOnCreated(){
+	this.veiws = new ReactiveVar(0); 
 });
